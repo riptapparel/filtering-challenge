7ce17df812bb469c7c0f78a755ec3bb6e3e3fd49
Brent Sanders <brent@sandersnewmedia.com>
2014-10-06 09:23:28 -0500

    adds more information on results

--- orders/tests.py
+++ orders/tests.py
@@ -2,22 +2,27 @@ from django.test import TestCase
 
 from orders.models import Order, OrderItem
 
+import results
+
 
 class OrderOrderingTestCase(TestCase):
     fixtures = ['test_orders.json']
 
     def test_orders_are_split_by_shipping_method(self):
-        #todo: write tests for fun and profit.
-        pass
+        #fcm, pri = Order.split_by_shipping_method()
+        self.assertEqual(results.fcm, fcm)
+        self.assertEqual(results.pri, pri)
 
     def test_orders_are_split_by_single_and_multiple(self):
-        #todo: write tests for fun and profit.
-        pass
+        #singles, multiples = Order.split_by_single_and_multiple()
+        self.assertEqual(results.singles, singles)
+        self.assertEqual(results.multiples, multiples)
 
     def test_single_orders_are_sorted(self):
-        #todo: write tests for fun and profit.
-        pass
+        #single_sorted_orders = Order.single_orders_are_sorted()
+        self.assertEqual(results.single_sorted_orders, single_sorted_orders)
 
     def test_multiple_orders_are_split_by_xxl_and_not(self):
-        #todo: write tests for fun and profit.
-        pass
+        #xxl, not_xxl = Order.orders_split_by_xxl_and_not()
+        self.assertEqual(results.xxl, xxl)
+        self.assertEqual(results.not_xxl, not_xxl)
